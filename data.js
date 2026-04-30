757a1dd687452ea3b3ca3ea4052e7d6e8c98d24a
DeepakBisht44 <bishtdeepak44158@gmail.com>
2026-04-30 23:45:59 +0530

    data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const projects = [
+    {
+        image: "images/project1.jpg",
+        title: "Face Recognition System"
+    },
+    {
+        image: "images/project2.jpg",
+        title: "Weather Prediction Model"
+    }
+];
+
+const assignments = [
+    {
+        name: "Assignment 1",
+        file: "assignments/assignment1.pdf"
+    },
+    {
+        name: "C Program",
+        file: "assignments/code.c"
+    }
+];
